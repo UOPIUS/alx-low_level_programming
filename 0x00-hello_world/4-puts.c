07f254788cbc636d8ff8ad5181c5770f2d3a3a66
Unogwu Pius <unogwupius@yahoo.com>
2022-09-08 21:04:59 -0700

    formatting output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  * main - entry point of the program
- * Description: to print exactly "Programming is like building a multilingual
- * puzzle
+ * 
  */
 int main(void) {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
