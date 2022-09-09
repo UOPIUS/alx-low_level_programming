dccba2fcc9c0d63a545dbbcd64ca4f7e4c8c2925
Unogwu Pius <unogwupius@yahoo.com>
2022-09-08 20:52:50 -0700

    Print text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * Description: to print exactly "Programming is like building a multilingual
+ * puzzle
+ */
+main() {
+	printf("Programming is like building a multilingual puzzle\n");
+}
