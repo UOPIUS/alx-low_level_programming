6e85ad4a775db8f535841e6f7e854bc936b81cd7
Unogwu Pius <unogwupius@yahoo.com>
2022-09-10 06:04:33 -0700

    cm

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - entry point of the program
+*
+* Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (int i = 0; i < 10; i++)
+		putchar('0' + i);
+	char = 'a';
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
