0469173b93a7c5498fc608693b2437a840948cca
Unogwu Pius <unogwupius@yahoo.com>
2022-09-09 21:21:04 -0700

    cm

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - entry point of the program
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
