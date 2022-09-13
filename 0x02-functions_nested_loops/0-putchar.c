b63dd25836af32d22db0ec3cfe81260a6e9631c3
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-13 21:20:25 +0100

    putchar modified

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 /**
-* main - entry point of the program
+* main - prints _putchar
 *
 * Return: 0
 */
 int main(void)
 {
-	int n;
-	int tmp;
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	tmp = n % 10;
 
-	if (tmp > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, tmp);
-	else if (tmp == 0)
-		printf("Last digit of %d is %d and is 0\n", n, tmp);
-	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, tmp);
-	return (0);
+return 0;
 }
