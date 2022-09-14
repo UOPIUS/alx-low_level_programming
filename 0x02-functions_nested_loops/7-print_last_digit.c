577aa78010f06ea8db72ba685a870cccf04f646f
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-14 01:10:14 +0100

    cm

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_last_digit - Prints last of a number
+* @c: number
+* Description: outputs the sign of a number
+* Return: Last digit of a number
+*/
+int print_last_digit(int c)
+{
+
+	return (c % 10);
+}
