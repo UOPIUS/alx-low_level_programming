c73c7defcde29383f7e8206b59be942c18a7926f
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-14 02:50:12 +0100

    cm

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* add - add 2 numbers
+* @a: number
+* @b: number
+* Description: summation of 2 nubers
+* Return: sum of 2 numbers
+*/
+int add(int a, int b);
+{
+	return (a+b);
+}
