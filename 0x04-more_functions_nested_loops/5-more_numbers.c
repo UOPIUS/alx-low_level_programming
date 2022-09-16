b14fe2e079971592e5a629b5ab9e67a3e532dc38
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-16 21:46:37 +0100

    cm

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@ for (i = 1; i <= 10; i++)
 j = 0;
 while (j <= 14)
 {
-if(j / 10)
-_putchar(j/10 + '0');
-_putchar(j%10 + '0');
+if (j / 10)
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 j++;
 }
 _putchar('\n');
