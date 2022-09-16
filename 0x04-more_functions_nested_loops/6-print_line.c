3299c43dbf9bfa546f1dcdc1f3545833bb3c651e
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-16 22:04:40 +0100

    cm

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_line -prints a line according to the number given
+*/
+void print_line(int len)
+{
+int i;
+for (i = len; i > 0; i--)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
