1c0b476e2834403a5e9fd04ba57c74af8a8be80b
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-16 21:41:42 +0100

    cm

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,15 +5,15 @@
 void more_numbers(void)
 {
 int i, j;
-for (i = 1; i < 10; i++)
+for (i = 1; i <= 10; i++)
 {
 j = 0;
-while (j < 10)
+while (j < 14)
 {
-_putchar(j + '0');
+_putchar(j/10 + '0');
+_putchar(j%10 + '0');
 j++;
 }
 _putchar('\n');
 }
-_putchar('\n');
 }
