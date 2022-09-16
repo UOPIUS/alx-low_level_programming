dbaacb06b172f6a9f4ea0f5cdd9f938b5676ce8a
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-16 23:05:23 +0100

    cm

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line according to the number given
+ * @len: length of expected line
+ */
+void print_diagonal(int len)
+{
+	int i, j;
+
+	if (len > 0)
+	{
+		for (i = 1; i <= len; i++)
+		{
+			if (i > 1)
+			{
+				j = 1;
+				while (j < i)
+				{
+					putchar('\t');
+					j++;
+				}
+			}
+			putchar('\\');
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
