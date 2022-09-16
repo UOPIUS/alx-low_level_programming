4bad135a8fb896d8ba91cb9c297538a05df93a81
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-16 23:17:02 +0100

    cm

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,24 +5,24 @@
  */
 void print_diagonal(int len)
 {
-	int i, j;
+int i, j;
 
-	if (len > 0)
-	{
-		for (i = 1; i <= len; i++)
-		{
-			if (i > 1)
-			{
-				j = 1;
-				while (j < i)
-				{
-					putchar(32);
-					j++;
-				}
-			}
-			putchar(92);
-			putchar('\n');
-		}
-	}
-	putchar('\n');
+if (len > 0)
+{
+for (i = 1; i <= len; i++)
+{
+if (i > 1)
+{
+j = 1;
+while (j < i)
+{
+putchar(32);
+j++;
+}
+}
+putchar(92);
+putchar('\n');
+}
+}
+putchar('\n');
 }
