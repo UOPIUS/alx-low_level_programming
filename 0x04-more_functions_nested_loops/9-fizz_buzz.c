3f9c8d90e0090dee4876e82fbf3975b42ab2d97a
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-17 01:31:46 +0100

    Buzz Fizz Buzzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,12 +9,12 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 > 0)
-printf("%s", "Fizz");
-else if (i % 5 == 0 && i % 3 > 0)
-printf("%s", "Buzz");
-else if (i % 3 == 0 && i % 5 == 0)
+if (i % 3 == 0 && i % 5 == 0)
 printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
 else
 printf("%d", i);
 putchar(' ');
