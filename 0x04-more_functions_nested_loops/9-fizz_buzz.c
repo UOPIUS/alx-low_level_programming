ad77aa766df894cf0e2ebfd3da0bac8b6422593c
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-17 02:21:48 +0100

    mfwsw

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 /**
- * main - print numbers
- * Return: integer
- */
+* main - print numbers
+* Return: integer
+*/
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = 0; i < 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz ");
-        else if (i % 3 == 0)
-            printf("Fizz ");
-        else if (i % 5 == 0)
-            printf("Buzz ");
-        else
-            printf("%d ", i);
-    }
-    printf("Buzz\n");
-    return (0);
+for (i = 0; i < 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", i);
+}
+printf("Buzz\n");
+return (0);
 }
