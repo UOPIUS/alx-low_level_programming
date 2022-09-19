64b0ed3b7442d09788c1dfd4b34059c5002cdf38
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-19 22:14:30 +0100

    cm

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _strlen - swaps the values of two integers.
+* and updates the value it points to to 98
+* @a: int
+* @b: int
+*/
+
+int _strlen(char *s)
+{
+return strlen(s);
+}
