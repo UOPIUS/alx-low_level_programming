9a8634a26453ae230d19cb0eb85fe3cc44442b95
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-20 04:05:30 +0100

    cm

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,8 @@ void puts2(char *str)
 int i, n = 0;
 while (str[n] != '\0')
 n++;
-for (i = 1; i <= n; i++)
-if (i % 2 == 1)
+for (i = 0; i < n; i++)
+if (i % 2 == 0)
 _putchar(str[i]);
 _putchar('\n');
 }
