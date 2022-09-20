c3b0e027fe9a41b66c00db18eed6d9fa3721ed39
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-20 03:53:37 +0100

    cm

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * rev_string - prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: characters
+ */
+
+void puts2(char *str)
+{
+int i, n = 0;
+while (str[n] != '\0')
+n++;
+for (i = 0; i < n; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
