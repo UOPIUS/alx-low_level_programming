e5b94ba02e4ea12cde62ae49a87f217423a08226
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-20 05:35:21 +0100

    cm

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
-* _strcpy -  copies the string pointed to by src,
+* *_strcpy - copies the string pointed to by src,
 * including the terminating null byte (\0), to the buffer pointed to by dest.
 * where n is the number of elements of the array to be printed
 * @dest: characters
 * @src: characters
+* Return: string
 */
 
 char *_strcpy(char *dest, char *src)
 {
 char *temp = dest;
-while (*src != '\0'){
+while (*src != '\0')
+{
 *dest = *src;
 dest++, src++;
 }
 *dest = '\0';
-return temp;
+return (temp);
 }
