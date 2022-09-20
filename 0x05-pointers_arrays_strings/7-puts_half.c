ed3311bcad47c86db5a103df551c0c5dc5db996c
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-20 04:39:41 +0100

    cm

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,18 @@
 
 void puts_half(char *str)
 {
-int i, half, n = 0;
+int i, half, temp, n = 0;
 
 while (str[n] != '\0')
 n++;
-if (n > 0){
+if (n > 0)
+{
 half = n / 2;
-for (i = half; i < n; i++)
+if (n % 2 != 0)
+i = half;
+else
+i = half + 1;
+for (; i < n; i++)
 _putchar(str[i]);
 }
 _putchar('\n');
