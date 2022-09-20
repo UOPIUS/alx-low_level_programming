b25f224528d0b4282a11383c291d8726a5743028
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-20 03:33:41 +0100

    cmp

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,15 +11,13 @@ char str;
 int i, n = 0;
 int len;
 char temp;
-/**
- * Get length of string
- */
+
 while (s[n] != '\0')
 n++;
-for (i = 0; i < n / 2; i++){
-    temp = s[i];
-    s[i] = s[n - 1 - i];
-    s[n - 1 - i] = temp;
+for (i = 0; i < n / 2; i++)
+{
+temp = s[i];
+s[i] = s[n - 1 - i];
+s[n - 1 - i] = temp;
 }
-
 }
