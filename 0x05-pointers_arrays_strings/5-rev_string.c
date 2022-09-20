ec36d67c997c501202960111126b7acfefd64b64
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-20 03:31:07 +0100

    cm

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: characters
+ */
+
+void rev_string(char *s)
+{
+char str;
+int i, n = 0;
+int len;
+char temp;
+/**
+ * Get length of string
+ */
+while (s[n] != '\0')
+n++;
+for (i = 0; i < n / 2; i++){
+    temp = s[i];
+    s[i] = s[n - 1 - i];
+    s[n - 1 - i] = temp;
+}
+
+}
