5f054f05b1613f8c3ae8fb092d270489a94b7528
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-22 20:04:26 +0100

    cm

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,12 @@
 
 void reverse_array(int *a, int n)
 {
-int i, temp;
-
-for (i = 0; i < n / 2; i++)
-{
-temp = a[i];
-a[n] = a[n - 1 - i];
-a[n - 1 - i] = temp;
-}
+	int i, temp;
+	
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[n] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
 }
