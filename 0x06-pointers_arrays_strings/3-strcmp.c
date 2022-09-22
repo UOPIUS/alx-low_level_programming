aafaa59a784a03a35d5d1341d898ffd10fefd2b0
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-22 18:30:19 +0100

    Compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* *_strcmp - compares 2 strings
+* @s1: characters
+* @s2: characters
+* Return: integer
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int ascii_difference;
+
+	while(*s1)
+	{
+		if(*s1 != *s2)
+			break;
+		s1++, s2++;
+	}
+	ascii_difference = *(unsigned char*)s1 - *(unsigned char*)s2;
+	printf("difference: %d",ascii_difference);
+	return (ascii_difference);
+}
