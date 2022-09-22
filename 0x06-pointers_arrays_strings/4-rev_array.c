3e739876de0d940dae160fda94655559ac45ce7e
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-22 20:12:19 +0100

    Fix errors in array reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,15 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, temp;
+	int *p, i, aux, k;
 
-	for (i = 0; i < n / 2; i++)
+	for (i = 0; i < n; i++)
+		p++;
+	for (k = 0; k < n / 2; k++)
 	{
-		temp = a[i];
-		a[n] = a[n - 1 - i];
-		a[n - 1 - i] = temp;
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
 	}
 }
