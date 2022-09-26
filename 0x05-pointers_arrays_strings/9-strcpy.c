7ab84a459c95a415427df1b83b850ba412a3d405
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-26 19:38:53 +0100

    After changing my token

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-char *temp = dest;
-while (*src != '\0')
-{
-*dest = *src;
-dest++, src++;
-}
-*dest = '\0';
-return (temp);
+	char *temp = dest;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++, src++;
+	}
+	*dest = '\0';
+	return (temp);
 }
