3c535f4ff355bf502447e52be3d03ef4cde67461
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-27 05:36:02 +0100

    cm

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character inside a string
+ * @s: string to check
+ * @c: character to check for
+ * Return: pointer to character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	return (0);
+}
