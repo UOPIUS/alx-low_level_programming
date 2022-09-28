95435f4cc3a473c246d47643d7a7c920027e9c98
Otseogbu Pius <unogwupius@yahoo.com>
2022-09-29 00:38:46 +0100

    cm

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string to print
+ */
+
+void _puts_recursion(char *s)
+{
+
+
+}
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverses a string
+ * @s: string to be passed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	} else
+		_putchar('\n');
+}
