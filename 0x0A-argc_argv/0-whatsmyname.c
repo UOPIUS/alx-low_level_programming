fc16c8b9255c5b8d6e0fb284291d31035d80338b
Otseogbu Pius <unogwupius@yahoo.com>
2022-10-03 12:42:48 +0100

    cm

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print program name
+ * @argc: argument counter
+ * @argv: argument list
+ * Return: integer
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+        printf("%s\n", *argv);
+        return (0);
+}
+
