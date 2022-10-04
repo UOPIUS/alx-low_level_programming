11d6721139f36b975acb7686f5ba7d7a27f19484
Otseogbu Pius <unogwupius@yahoo.com>
2022-10-04 05:57:37 +0100

    cm

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * xstrlen - length of a string
+ * @s: string
+ * Return: length of the string
+ */
+int xstrlen(char *s)
+{
+	int l = 0;
+	while (s != '\0')
+		l++;
+	return l;
+}
+/**
+ *
+ * argstostr - concats all the arguments of a program
+ * @ac: number of arguments
+ * @av: argument array
+ * Return: pointer
+ */
+
+char *argstostr(int ac, char **av)
+{
+	if (ac == 0 || av = NULL)
+		return (NULL);
+	int i = ac, k = 0, sum = 0, temp = 0, j = 0;
+
+}
