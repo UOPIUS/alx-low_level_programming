5af8d5ffb5146cb7098a5c9c56201ae766d6f315
Otseogbu Pius <unogwupius@yahoo.com>
2022-10-13 15:15:45 +0100

    cm

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print all numbers followed by a new line
+ * @separator: separator
+ * @n: number of integers passed to the function
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int k = 0;
+	va_list args;
+
+	if (*separator && n != 0)
+	{
+		va_start(args, n);
+		for (; k < n; k++)
+		{
+			printf("%d", va_arg(args, int));
+			if (k < n - 1)
+				putchar(*separator);
+		}
+	}
+}
