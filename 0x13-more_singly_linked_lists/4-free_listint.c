a7cd02e9246875af1686a1ca168cf5faa434657b
Otseogbu Pius <unogwupius@yahoo.com>
2022-10-25 15:45:46 +0100

    cm

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,6 +5,14 @@
  * @head: listint_t list
  */
 void free_listint(listint_t *head)
-{
 
+{
+	listint_t *node;
+
+	while (head != NULL)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
 }
