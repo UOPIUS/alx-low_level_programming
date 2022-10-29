9675e5832cd0a322694b4839826bc409df8f2ac0
Otseogbu Pius <unogwupius@yahoo.com>
2022-10-29 11:03:29 +0100

    cm

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -24,7 +24,7 @@ unsigned int binary_to_uint(const char *b)
 	int i, dec, next = 0;
 
 	i = stlen(b) - 1;
-	for (dec = 0; i >=0; --i, ++next)
+	for (dec = 0; i >= 0; --i, ++next)
 	{
 		if ((b[i] == '0') || (b[i] == '1'))
 			dec = dec + (b[i] - 48) * (1 << next);
